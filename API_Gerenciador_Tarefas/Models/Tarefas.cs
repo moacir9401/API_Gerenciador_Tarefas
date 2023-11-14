1b2fded9ab47e5935f8ca8d2780a473395528df6
moacir <moacir9401@gmail.com>
2023-11-14 19:07:24 -0300

    Adicionar arquivos de projeto.

---
+++ API_Gerenciador_Tarefas/Models/Tarefas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Gerenciador_Tarefas.Models
+{
+    [Table(nameof(Tarefas))]
+    public class Tarefas
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+
+       [ForeignKey(nameof(UsuarioId))]
+        public Usuario Usuario { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataConclusao { get; set; }
+    }
+}
