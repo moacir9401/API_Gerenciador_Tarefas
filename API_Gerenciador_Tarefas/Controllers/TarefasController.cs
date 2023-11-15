a35f54147e949d4b19579092d68512e58139657b
moacir <moacir9401@gmail.com>
2023-11-14 22:37:31 -0300

    implementado tarefasController

---
+++ API_Gerenciador_Tarefas/Controllers/TarefasController.cs
@@ -0,0 +1,85 @@
+﻿using API_Gerenciador_Tarefas.Models;
+using API_Gerenciador_Tarefas.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Gerenciador_Tarefas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefasController : Controller
+    {
+
+        private readonly ApiContext _context;
+ 
+        public TarefasController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAll")]
+
+        public async Task<List<Tarefas>> GetAll()
+        {
+            return await _context.Tarefas.ToListAsync();
+        }
+        [HttpGet]
+        public async Task<Tarefas?> Get(int? id)
+        {
+            if (id == null)
+                throw new ArgumentException("Id não preenchido");
+
+            return await _context.Tarefas.FirstOrDefaultAsync(u => u.Id == id);
+        }
+        [HttpPost]
+        public async Task Create(Tarefas Tarefa)
+        {
+            _context.Tarefas.Add(Tarefa);
+            await _context.SaveChangesAsync();
+        }
+        [HttpPut]
+        public async Task<Tarefas> Update(Tarefas Tarefa)
+        {
+            if (Tarefa == null)
+                throw new ArgumentException("Tarefas não preenchido");
+
+            _context.Tarefas.Update(Tarefa);
+            await _context.SaveChangesAsync();
+
+            return Tarefa;
+        }
+        [HttpDelete]
+        public async Task Delete(int? id)
+        {
+            if (id == null)
+                throw new ArgumentException("Id não preenchido");
+
+            var Tarefa = await Get(id);
+
+
+            if (Tarefa == null)
+                throw new ArgumentException("Tarefas não encontrado");
+
+            _context.Tarefas.Remove(Tarefa);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Tarefas>> GetTarefasToConluidas()
+        {
+            return await _context.Tarefas.Where(t => t.Concluida).ToListAsync();
+        }
+        public async Task<List<Tarefas>> GetTarefasToNaoConluidas()
+        {
+            return await _context.Tarefas.Where(t => !t.Concluida).ToListAsync();
+        }
+
+        public async Task<List<Tarefas>> PeriodoConclusao(DateTime dataInicio, DateTime dataTermino)
+        {
+             return await _context.Tarefas.Where(t => t.DataConclusao >= dataInicio && t.DataConclusao <= dataTermino).ToListAsync();
+        }
+        public async Task<List<Tarefas>> PeriodoVencimento(DateTime dataInicio, DateTime dataTermino)
+        {
+             return await _context.Tarefas.Where(t => t.DataVencimento >= dataInicio && t.DataVencimento <= dataTermino).ToListAsync();
+        }
+    }
+}
