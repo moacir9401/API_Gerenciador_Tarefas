d5c27bdcae953745cbe23b2b11c3a1416ab48c4f
moacir <moacir9401@gmail.com>
2023-11-14 21:50:13 -0300

    configurando o banco de dados

---
+++ API_Gerenciador_Tarefas/Models/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Gerenciador_Tarefas.Models.Context
+{
+    public class ApiContext:DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> option) : base(option)
+        {
+                
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarefas> Tarefas { get; set; }
+    }
+}
--- API_Gerenciador_Tarefas/Program.cs
+++ API_Gerenciador_Tarefas/Program.cs
@@ -1,11 +1,20 @@
+using API_Gerenciador_Tarefas.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+builder.Services.AddDbContext<ApiContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion
+    (new Version(8,0,35))));
 
 var app = builder.Build();
 
